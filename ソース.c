739f8a69b7f3384c811fdfea67d2d4c68ca8a11e
H.Mizuta <39897014+miztahiroto@users.noreply.github.com>
2022-04-13 01:03:30 +0900

    Add files via upload

--- ソース.c
+++ ソース.c
@@ -1,93 +1,79 @@
 #pragma warning(disable: 4996)
 #include <stdio.h>
+#include <string.h>
 
-/* 平均 */
-/*
-void ave(double ave) {
-    FILE* f = fopen("test.tsv", "r");
-    char line[256];
-    char name[256];
-    int score[3]={0};
-
-    while (fgets(line, 256, f) != NULL) {
-        int total=0;
-        for (int i = 1; i <= 3;i++) {
-            total = total + score[i];
-        }
-        printf("名前:%s\n", name);
-        printf("合計:%d\n", total);
-
-        ave= total / 4;
-        printf("名前:%s\n", name);
-        printf("名前:%.2f\n", ave);
-    }
-    fclose(f);
-
-}
-*/
+struct schedule {
+    int year;        /* 年 */
+    int month;       /* 月 */
+    int day;         /* 日 */
+    int hour;        /* 時 */
+    char title[100]; /* 予定 */
+};
 
 int main() {
+    struct schedule entry;
 
-    FILE* f = fopen("test.tsv", "r");
-    char line[256];
-    char name[256];
-    int score[4];
-
-
-
-    while (fgets(line, 256, f) != NULL) {
-        (void)sscanf(line, "%s\t%d\t%d\t%d\t%d\t", &name, &score[0], &score[1], &score[2], &score[3]);
-        //       for (int i = 0; i <= 3; i++) {
-
-        //           printf("名前:%s\n", name);
-        //           printf("%d点, \n", score[i]);
-        for (int i = 0; i <= 3; i++) {
-            for (int j= i+1; j <= 3; j++) {
-                
-                if (score[i] > score[j]) {
-                    int tmp = score[i];
-                    score[i] = score[j];
-                    score[j] = tmp;
-                }
-                
-            }
-            printf("%d, ",score[i]);
-        }
-        printf("\n");
+	/*ファイル初期化*/
+	FILE* g = fopen("test.txt", "w");
+	fprintf(g, "");
+	fclose(g);
 
 
-        int total = 0;
-        for (int i = 0; i <= 3; i++) {
-            total += score[i];
-        }
-        printf("合計:%d\n", total);
-        double ave = 0;
-        ave = (double)total / 4;
-        printf("平均:%.2f\n", ave);
-        /*
-        int total = 0;
-        total = score[0] + score[1] + score[2];
-        printf("国語:%d点 英語:%d点 数学:%d点 物理:%d点\n", score[0], score[1], score[2], score[3]);
-        printf("合計:%d\n", total);
+	/*ファイルに書き込み(max10行)*/
+	for(int i=1;i<=10;i++){
 
-        double ave = 0;
-        ave = (double)total / 4;
-        printf("平均:%.2f\n", ave);
-        */
+		FILE* h = fopen("test.txt", "a");
 
-        /*
-        sscanf(line, "%s\t%d\t%d\t%d\t%d\t", &name, &score[0], &score[1], &score[2], &score[3]);
-        printf("名前:%s\n", name);
-        printf("国語:%d点 英語:%d点 数学:%d点 物理:%d点\n", score[0], score[1], score[2], score[3]);
-        */
+		int input = 0;
+		printf("スケジュールを入力する場合は1を、しない場合は0を入力してください。");
+		(void)scanf("%d", &input);
+		if (input == 1) {
 
+			int year;
+			int month;
+			int hour;
+			int day;
+			char title[256];
 
+			printf("年を入力してください: ");
+			(void)scanf("%d", &year);
+			printf("月を入力してください: ");
+			(void)scanf("%d", &month);
+			printf("日を入力してください: ");
+			(void)scanf("%d", &day);
+			printf("時を入力してください: ");
+			(void)scanf("%d", &hour);
+			printf("タイトル入力してください: ");
+			(void)scanf("%s", &title);
 
-        printf("\n");
-    }
+			entry.year = year;
+			entry.month = month;
+			entry.day = day;
+			entry.hour = hour;
+			strcpy(entry.title, title);
 
-    fclose(f);
-    //    printf("平均:%.2f\n",ave(ave);
+			printf("%d : %04d/%02d/%02d %02d:00 %s\n\n",
+				i, entry.year, entry.month, entry.day, entry.hour, entry.title);
 
+			fprintf(h, "%d : %04d/%02d/%02d %02d:00 %s\n",
+				i, entry.year, entry.month, entry.day, entry.hour, entry.title);
+		}
+		fclose(h);
+
+		/*入力を終了するとき*/
+		if (input == 0 || i==10) {
+			printf("スケジュール入力を終了します。\n");
+			printf("書き込まれたスケジュールは下記です。\n\n");
+
+			FILE* f = fopen("test.txt", "r");
+
+			char line[256];
+			while (fgets(line, 256, f) != NULL) {
+				printf("%s", line);
+			}
+			fclose(f);
+			break;
+		}
+	}
     return 0;
 }
