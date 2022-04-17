1a8653c5cf91f52671d024eff81f0bd5d8013b78
H.Mizuta <39897014+miztahiroto@users.noreply.github.com>
2022-04-18 01:33:44 +0900

    Add files via upload

--- ソース.c
+++ ソース.c
@@ -1,96 +1,70 @@
-#pragma warning(disable: 4996)
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <memory.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define MAX 3
-
-typedef struct {
-	int cnt; // 文字数
-	char moji;//文字列
-} MOJI;
-
-// qsortで判定するための関数。別に難しいことはしてない。aとbを比較して大きければ正の値を返せば昇順になる。
-// 今回はテストの点なので値が大きい順（降順）だから、if文内の判定を逆にしている。
-int compar(const void* a, const void* b) {
-	if (((MOJI*)a)->cnt < ((MOJI*)b)->cnt) {
-		return 1;
-	}
-	else {
-		return -1;
-	}
+//char型でメモリ確保
+char* MallocChar(long size){
+	char* p;
+	if (!(p = (char*)malloc(size)))
+		return NULL;
+	memset(p, 0, size);
+	return p;
 }
 
-
-
-
-void des(char moji[MAX][256], char tmp[256], int cnt[10]) {
-	//	char tmp[256];
-	for (int i = 0; i < MAX; i++) {
-		for (int j = i + 1; j < MAX; j++) {
-			if (cnt[i] < cnt[j]) {
-				//				char tmp = cnt[i];
-								//cnt[i] = cnt[j];
-								//cnt[j] = tmp;
-
-
-
-				strcpy(tmp, moji[i]);
-				printf("%s, \n", tmp);
-
-				strcpy(moji[i], moji[j]);
-				//				printf("%s, \n", moji[i]);
-
-				strcpy(moji[j], tmp);
-				//				printf("%s, \n", moji[j]);
-			}
-		}
-		//	printf("%d, \n",  cnt[i]);
-		//	printf("%s, \n", moji[i]);
-	}
-
-	printf("入力文字列を文字数の多い順に並び変えます\n");
-	for (int i = 0; i < MAX; ++i) {
-		printf("%d. 文字数%d: 文字列%s\n", i + 1, cnt[i], moji[i]);
-	}
-
-
+//メモリ内の文字を表示
+void Print(const char* p, long size){
+	for (int i = 0; i < size; i++)
+		printf("%c", *p++);
 }
 
+//ファイルサイズ取得
+int GetFileSize(int f) {
+	struct stat sb;
+	fstat(f, &sb);
+	return sb.st_size;
+}
 
+int main(){
+	const char* file = "test.txt";
+	FILE* fp;
+	int size;
+	char* p;
 
-int main(void) {
-	char moji[MAX][256];
-	char tmp[256];
-	int cnt[MAX];
+//	int fd = fopen(file, "r");
+	fopen_s(&fp, file, "rb");
+//	FILE* fp = fopen("test.txt", "r");
+	int fd = open(file, O_RDONLY);
 
-	MOJI youso[MAX] ;
+	//if (fp == NULL)	{
+//		char message[300];
+	//	snprintf(message, 300, "%sは存在しません。\n", file);
+	//	Quit(message);
+	//}
 
-	//キーボードから文字列を入力 
-	for (int i = 0; i < MAX; i++) {
-		printf("文字列%d:", i + 1);
-		//入力文字をアドレスの文字に入力
-		(void)scanf("%s", moji[i]);
-
-		youso[i].moji = moji;
-		youso[i].cnt= (int)strlen(youso[i].moji);
-		//printf("a%s\n", moji);
-
+	size = GetFileSize(fd);
+	if (size <= 0){
+		printf("ファイルサイズを取得できませんでした。\n");
+	}
+	if (size > 0) {
+		printf("ファイルサイズは%dバイトです。\n\n", size);
+		printf("以下、ファイルの内容です。\n");
 	}
 
-	// ソート前
-	for (int i = 0; i < MAX; i++) {
-		printf("文字数: % d, 文字列 : % s\n", youso[i].cnt, youso[i].moji);
+	p = MallocChar(size);
+	if (!p){
+		printf("メモリの確保に失敗しました。\n");
 	}
 
-	// 成績でソート
-	qsort(youso, MAX, cnt, compar);
+	fread(p, size, 1, fp);
 
-	// ソート後
-	printf("\n->\n\n");
-	for (int i = 0; i < 10; i++) {
-		printf("出席番号:%d, 成績:%d\n", youso[i].cnt, youso[i].moji);
-	}
+	fclose(fp);
 
-//	des(moji, tmp, cnt);
+	//表示
+	Print(p, size);
+
+	free(p);
+
+	return 0;
 }
