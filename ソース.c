4653d1728d0a2fb54b0a5a19fa4e05ce18048824
H.Mizuta <39897014+miztahiroto@users.noreply.github.com>
2022-04-19 02:52:45 +0900

    Add files via upload

--- ソース.c
+++ ソース.c
@@ -1,66 +1,159 @@
+#pragma warning(disable: 4996)
 #include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
 
-#define _POSIX_SOURCE
+int main() {
 
-//char型でメモリ確保
-char* MallocChar(long size){
-	char* p;
-	if (!(p = (char*)malloc(size)))
-		return NULL;
-	memset(p, 0, size);
-	return p;
-}
+	FILE* fp = fopen("test.txt", "r");
+	/*
+	while ((c = getc(fp)) != EOF) {
+		//* c がアルファベットの小文字か大文字なら 
+		if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
+			alpha[c]++;        //* カウントする 
+		}
+		printf("%c\n",c);
+	}
+	*/
+	char a[] = { "ABBBSAaaaacccgv" };
+	int cnt[52] = { 0 };
 
-//メモリ内の文字を表示
-void Print(const char* p, long size){
-	for (int i = 0; i < size; i++)
-		printf("%c", *p++);
-}
+	int len = (int)strlen(a);
 
-//ファイルサイズ取得
-int GetFileSize(int f) {
-	struct stat sb;
-	fstat(f, &sb);
-	return sb.st_size;
-}
+	char line[256];
+	while (fgets(line, 256, fp) != NULL) {
+//		printf("%s", line);
+	}
+//	printf("\n\n");
+	//printf("%s\n", line[0]);
+//	printf("%s\n", line);
+	//int len= strlen(line);
+	//printf("%d", len);
 
-int main(){
-	const char* file = "test.txt";
-	FILE* fp;
-	int size;
-	char* p;
+	//char line[256];
+	//while (fgets(line, 256, f) != NULL) {
+	//	printf("%s", line);
+	//}
 
-//	int fd = fopen(file, "r");
-	fopen_s(&fp, file, "rb");
-//	FILE* fp = fopen("test.txt", "r");
-	int fd = open(file, O_RDONLY);
+	//大文字、小文字を配列に入れる
+	char komoji[26] = { 0 };
+	char oomoji[52] = { 0 };
 
-	size = GetFileSize(fd);
-	if (size <= 0){
-		printf("ファイルサイズを取得できませんでした。\n");
+	int i = 0;
+	for (int c = 'A'; c <= 'Z'; c++) {
+		komoji[i] = c;
+		printf("%c\t", komoji[i]);
+		i = i++;
+	}
+	
+	int j = 26;
+	for (int c = 'a'; c <= 'z'; c++) {
+		oomoji[j] = c;
+		printf("%c\t", oomoji[j]);
+		j = j++;
+	}
+	
+	//カウント
+	for (int k = 0; k < len; k++) {
+		if (a[k] >= 'A' && a[k] <= 'Z') {
+			cnt[(a[k]- 'A') ]++;
+		}
+		if (a[k] >= 'a' && a[k] <= 'z'){
+			cnt[(a[k] - 'a') + 26]++;
+		}
 	}
 
-	p = MallocChar(size);
-	if (!p){
-		printf("メモリの確保に失敗しました。\n");
+	printf("文字の出現回数\n");
+	for (int k = 0; k < 26; k++) {
+		printf(" % c : % d\n", komoji[k], cnt[k]);
+	}
+	for (int k = 26; k < 52; k++) {
+		printf(" % c : % d\n", oomoji[k], cnt[k ]);
 	}
 
-	if (size > 0) {
-		printf("ファイルサイズは%dバイトです。\n\n", size);
-		printf("以下、ファイルの内容です。\n");
-		
-		fread(p, size, 1, fp);
-
-		//表示
-		Print(p, size);
-		
-		fclose(fp);
+	char moji[256];
+	for (int k = 0; k < 26; k++) {
+		moji[k] = komoji[k];
+		printf(" %c\n", komoji[k]);
 	}
-	free(p);
+	for (int k = 26; k < 52; k++) {
+		moji[k] = oomoji[k];
+		printf(" %c\n" ,oomoji[k]);
+	}
+
+
+	//sort
+	char tmp[256];
+	for (int k = 0; k < 52; k++) {
+		for (int l = k + 1; l < 52; l++) {
+			if (cnt[k] < cnt[l]) {
+				int tmp2 = 0;
+				//tmp2= alpha[fig[k]];
+				//alpha[fig[k]] = alpha[fig[j]];
+				//alpha[fig[j]] = tmp;
+				tmp2 = cnt[k];
+				cnt[k] = cnt[l];
+				cnt[l] = tmp2;
+
+				strcpy(tmp,moji[k]);
+				strcpy(moji[k], moji[l]);
+				strcpy(moji[l], tmp);
+			}
+		}
+	}
+
+
+
+	/*
+	char fig[26] = { 0 };
+
+	int i = 0;
+	for (c = 'a'; c <= 'z'; c++) {
+		fig[i] = c;
+		printf("%c\t", fig[i]);
+		i = i++;
+	}
+	printf("\n\n");
+
+
+	for (c = 'a'; c <= 'z'; c++) { //小文字の出現頻度を 
+		printf("%c:%d\t", c, alpha[c]);
+	}
+	
+	printf("\n\n");
+	
+	for (c = 'A'; c <= 'Z'; c++){    // 大文字の出現頻度を 
+		printf("%c:%d\t", c, alpha[c]);        
+	}
+	printf("\n");
+
+	printf("%d \t", alpha[c]);
+	
+	//sort
+	char tmp[256]={0};
+	for (int k = 0; k < 26; k++) {
+		for (int j = k + 1; j < 5; j++) {
+			if (alpha[fig[k]] < alpha[fig[j]]) {
+				int tmp2 = 0;
+				//tmp2= alpha[fig[k]];
+				//alpha[fig[k]] = alpha[fig[j]];
+				//alpha[fig[j]] = tmp;
+				tmp2 = alpha[k];
+				alpha[k] = alpha[j];
+				alpha[j] = tmp;
+
+				//strcpy(tmp,fig[k]);
+				//strcpy(fig[k], fig[j]);
+				//strcpy(fig[j], tmp);
+			}
+		}
+	}
+	*/
+	for (int k = 0; k < 52; k++) {
+		printf("%d,%c \t", cnt[k],moji[k]);
+	}
+	printf("\n\n");
+
+	fclose(fp);
 
 	return 0;
 }
